004799cc2fde220a2ed253c061e5a8fb3028eb5d
alex <alex-p14@bk.ru>
2023-04-06 21:39:03 +0300

    Update file tests/StringUtilsTest.php
    Add autoload directory.

--- tests/StringUtilsTest.php
+++ tests/StringUtilsTest.php
@@ -1,12 +1,20 @@
 <?php
 
-require_once 'src/StringUtils.php';
+// Путь который будет использован при глобальной установке пакета
+$autoloadPath1 = __DIR__ . '/../../../autoload.php';
+// Путь для локальной работы с проектом
+$autoloadPath2 = __DIR__ . '/../vendor/autoload.php';
 
-if (\StringUtils\capitalize('hello') !== 'Hello') {
-	throw new \Exception("Функция работает неверно!\n");
+if (file_exists($autoloadPath1)) {
+    require_once $autoloadPath1;
+} else {
+    require_once $autoloadPath2;
 }
-if (\StringUtils\capitalize('') !== '') {
-	throw new \Exception("Функция работает неверно!\n");
-}
+
+use Webmozart\Assert\Assert;
+use function StringUtils\capitalize;
+
+Assert::eq(capitalize('hello'), 'Hello', 'Ошибка!');
+Assert::eq(capitalize(''), '', 'Ошибка!');
 
 echo "Все тесты успешно пройдены!\n";
